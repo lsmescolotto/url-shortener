1acf20912bc7b07821f6902f335d25c06fbf257e
Luiza <lsmescolotto@gmail.com>
2026-09-03 16:31:20 -0300

    feat: post fn with early return and improved validation

--- Program.cs
+++ Program.cs
@@ -18,64 +18,62 @@ app.UseHttpsRedirection();
 
 app.MapPost("/urls", (CreateUrlRequest request) => {
   Console.WriteLine($"Received request to shorten URL: {request.Url}");
-  if (Uri.TryCreate(request.Url, UriKind.Absolute, out Uri? uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
-  {
-    if (request.ExpirationInMinutes > 0 || !request.ExpirationInMinutes.HasValue)
-    {
-      string code;
-      if (string.IsNullOrEmpty(request.ShortCode))
-      {
-        code = Guid.NewGuid().ToString().Substring(0, 7);
-      }
-      else if (request.ShortCode.Length > 3 && request.ShortCode.Length <= 20)
-      {
-        code = request.ShortCode;
-      }
-      else
-      {
-        return Results.BadRequest(new { message = "ShortCode must be between 4 and 20 characters long." });
-      }
-
-
-      Console.WriteLine($"URI: {uriResult}");
-      string id = Guid.NewGuid().ToString();
-
-      while (urlsList.ContainsKey(code))
-      {
-        if (!string.IsNullOrEmpty(request.ShortCode))
-        {
-          return Results.Conflict(new { message = "Short code already exists." });
-        }
-        else
-        {
-          code = Guid.NewGuid().ToString().Substring(0, 7);
-        }
-      }
-
-      var urlResponse = new UrlResponse(
-        id,
-        uriResult.ToString(),
-        code,
-        DateTime.UtcNow,
-        DateTime.UtcNow.AddMinutes(request.ExpirationInMinutes ?? 60),
-        request.ExpirationInMinutes ?? 60,
-        0
-      );
-
-      urlsList.TryAdd(code, urlResponse);
-
-      return Results.Created($"/urls/{code}", urlResponse);
-    }
-
-    else
-    {
-      return Results.BadRequest(new { message = "ExpirationInMinutes must be greater than 0." });
-    }
-  }
-  else
+  if (!Uri.TryCreate(request.Url, UriKind.Absolute, out Uri? uriResult) ||
+  !(uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
   {
     return Results.BadRequest(new { message = "Invalid URL format." });
   }
+
+  if (request.ExpirationInMinutes <= 0 && request.ExpirationInMinutes.HasValue)
+  {
+    return Results.BadRequest(new { message = "ExpirationInMinutes must be greater than 0." });
+  }
+
+  string code;
+
+  if (request.ShortCode != null)
+  {
+    if (string.IsNullOrWhiteSpace(request.ShortCode))
+    {
+      return Results.BadRequest(new { message = "ShortCode must not be empty or contain only whitespace." });
+    }
+    else if (request.ShortCode.Length < 4 || request.ShortCode.Length > 20)
+    {
+      return Results.BadRequest(new { message = "ShortCode must be between 4 and 20 characters long." });
+    }
+    code = request.ShortCode;
+
+  }
+  else
+  {
+    code = Guid.NewGuid().ToString().Substring(0, 7);
+  }
+
+  Console.WriteLine($"URI: {uriResult}");
+  string id = Guid.NewGuid().ToString();
+
+  while (urlsList.ContainsKey(code))
+  {
+    if (!string.IsNullOrEmpty(request.ShortCode))
+    {
+      return Results.Conflict(new { message = "Short code already exists." });
+    }
+    code = Guid.NewGuid().ToString().Substring(0, 7);
+  }
+
+  var urlResponse = new UrlResponse(
+    id,
+    uriResult.ToString(),
+    code,
+    DateTime.UtcNow,
+    DateTime.UtcNow.AddMinutes(request.ExpirationInMinutes ?? 60),
+    request.ExpirationInMinutes ?? 60,
+    0
+  );
+
+  urlsList.TryAdd(code, urlResponse);
+
+  return Results.Created($"/urls/{code}", urlResponse);
 });
 
 app.MapGet("/urls/{code}", (string code) => {
